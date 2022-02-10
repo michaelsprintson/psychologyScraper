99c1b6ee8293c4e9546be83b9334b7736ca34e2e
michaelsprintson <ms148@rice.edu>
2022-02-09 18:18:40 -0600

    new timing system, new saving system

--- TwitterScraper.py
+++ TwitterScraper.py
@@ -1,9 +1,12 @@
-import config
+from . import config
 import requests
 import json
 import csv
-import TextAnalyzer
+from . import TextAnalyzer
 import time
+import datetime as DT
+from dateutil import parser
+from tqdm import tqdm
 
 ## General information: This piece of code sends requests to twitter about the desired keywords. If found, it is written in a csv file.
 
@@ -15,6 +18,7 @@ def create_url(keyword):
                     # 'start_time': start_date,
                     'max_results': 100,
                     'user.fields': 'id,name,username',
+                    'tweet.fields': 'created_at',
                     'expansions' : 'author_id',
                     'next_token': {}}
     return (search_url, query_params)
@@ -23,7 +27,7 @@ def create_url(keyword):
 def connect_to_endpoint(url, headers, params, next_token = None):
     params['next_token'] = next_token
     response = requests.request("GET", url, headers = headers, params = params)
-    print("Endpoint Response Code: " + str(response.status_code))
+    # print("Endpoint Response Code: " + str(response.status_code))
     if response.status_code != 200:
         raise Exception(response.status_code, response.text)
     return response.json()
@@ -34,7 +38,7 @@ def createSearchStrings():
     currentString = ""
     for keyword in TextAnalyzer.textValuesDict:
         next_search = f'({keyword.replace(" ", "%20")})'
-        if len(currentString) + len(next_search) + 6 > 128: 
+        if len(currentString) + len(next_search) + 6 > 450: 
             searchStrings.append(currentString)
             currentString = ""
         if currentString != "":
@@ -44,30 +48,45 @@ def createSearchStrings():
     return searchStrings
 
 def runTwitterScraper():
-    valid_responses = []
+
+    today = DT.datetime.now(DT.timezone.utc)
+    # week_ago = today - DT.timedelta(days=7)
+
     responseCount = 1
-    for searchString in createSearchStrings():
-        url = create_url(searchString)
-        continueFlag = True
-        nextToken = {}
-        while continueFlag:
-            time.sleep(5)
-            json_response = connect_to_endpoint(url[0], {"Authorization": "Bearer {}".format(config.twitter_bearer_token)}, url[1], nextToken)
-            print(f'Response page: ${responseCount}')
-            print(f'Response metadata: ${json_response["meta"]}')
-            if "data" in json_response:
-                for response in json_response["data"]:
-                    if (TextAnalyzer.textPasses(response["text"])):
-                        valid_responses.append(response)
-            if "next_token" not in json_response["meta"]:
-                continueFlag = False
-            else:
-                nextToken = json_response["meta"]["next_token"] 
+    with tqdm(total=604800) as pbar:
+        for searchString in createSearchStrings():
+            url = create_url(searchString)
+            continueFlag = True
+            nextToken = {}
+            while continueFlag:
+                time.sleep(5)
+                json_response = connect_to_endpoint(url[0], {"Authorization": "Bearer {}".format(config.twitter_bearer_token)}, url[1], nextToken)
+                page_oldest_date = parser.parse(([i for i in json_response['data'] if i['id'] == json_response['meta']['oldest_id']][0]['created_at']))
+                final_val = (today - page_oldest_date).total_seconds()
+                pbar.update(int(final_val) - pbar.n)
 
-    csvFile = open("data.csv", "a", newline="", encoding='utf-8')
-    csvWriter = csv.writer(csvFile)
+                rc = json_response['meta']['result_count']
+                if int(rc) < 90:
+                    print(f"Response page: {responseCount}, {rc}")
+                
+                if "data" in json_response:
+                    for response in json_response["data"]:
+                        if (TextAnalyzer.textPasses(response["text"])):
+                            # print(response, '\n\n')
+                            with open("test.txt", "a") as myfile:
+                                myfile.write(response.__repr__() + "\n")
+                            responseCount += 1
+                if "next_token" not in json_response["meta"]:
+                    print("stopped, no next token")
+                    continueFlag = False
+                else:
+                    if responseCount > 10:
+                        continueFlag = False
+                    nextToken = json_response["meta"]["next_token"] 
 
-    for validResponse in valid_responses:
-        csvWriter.writerow(validResponse)
     
-    csvFile.close()
+
+    # for validResponse in valid_responses:
+    #     csvWriter.writerow(validResponse)
+    
+    
